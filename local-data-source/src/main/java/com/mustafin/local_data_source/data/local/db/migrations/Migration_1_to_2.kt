a57e3df4d690dbe5b66f932da467e01dff7c9e86
T-e-i-l-s <kmoustafin@yandex.ru>
2025-04-27 09:34:44 +0300

    DB Migration Fix

--- local-data-source/src/main/java/com/mustafin/local_data_source/data/local/db/migrations/Migration_1_to_2.kt
+++ local-data-source/src/main/java/com/mustafin/local_data_source/data/local/db/migrations/Migration_1_to_2.kt
@@ -7,6 +7,7 @@ import com.mustafin.local_data_source.data.local.db.Tables
 val MIGRATION_1_2 = object : Migration(1, 2) {
     override fun migrate(db: SupportSQLiteDatabase) {
         db.execSQL("ALTER TABLE ${Tables.REQUESTS_TABLE} ADD COLUMN responseStatuses TEXT")
-        db.execSQL("""UPDATE ${Tables.REQUESTS_TABLE} SET responseStatuses = json('[]')""".trimIndent())
+        db.execSQL("""UPDATE ${Tables.REQUESTS_TABLE} SET responseStatuses = '[]'""")
+        db.execSQL("""UPDATE ${Tables.REQUESTS_TABLE} SET lastResponseStatus = NULL""")
     }
 }
